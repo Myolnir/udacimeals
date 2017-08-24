9f9ec5be96149432c39df298e217e82f2fa21b22
Angel Muelas <angel.muelas@gmail.com>
2017-08-24 08:03:44 +0200

    fix: fixed wrong import

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
+import devToolsEnhancer from 'remote-redux-devtools';
+
 
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  devToolsEnhancer()
 );
 
 ReactDOM.render(
