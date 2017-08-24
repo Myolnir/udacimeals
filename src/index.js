c9d9882cffc854c71d65c94f27e5f352a1a1f8c8
Angel Muelas <angel.muelas@gmail.com>
2017-08-24 08:04:46 +0200

    fix: revert dev tools change

--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import reducer from './reducers';
-import devToolsEnhancer from 'remote-redux-devtools';
-
 
 const store = createStore(
   reducer,
-  devToolsEnhancer()
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 ReactDOM.render(
