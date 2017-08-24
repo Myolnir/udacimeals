ffab224d2386f973abcf3eaebefb1bbf7c344730
Angel Muelas <angel.muelas@gmail.com>
2017-08-24 07:54:01 +0200

    feat: initial version of react and redux working together

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,44 @@
-import React, { Component } from 'react';
-import logo from '../logo.svg';
-import '../App.css';
+import React, { Component } from 'react'
+import { addRecipe } from '../actions'
 
 class App extends Component {
+  state = {
+    calendar: null
+  }
+  componentDidMount () {
+    const { store } = this.props
+
+    store.subscribe(() => {
+      this.setState(() => ({
+        calendar: store.getState()
+      }))
+    })
+  }
+  submitFood = () => {
+    this.props.store.dispatch(addRecipe({
+      day: 'monday',
+      meal: 'breakfast',
+      recipe: {
+        label: this.input.value
+      },
+    }))
+
+    this.input.value = ''
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <input
+          type='text'
+          ref={(input) => this.input = input}
+          placeholder="Monday's Breakfast"
+        />
+        <button onClick={this.submitFood.bind(this)}>Submit</button>
+        <pre>
+          Monday's Breakfast: {this.state.calendar && this.state.calendar.monday.breakfast}
+        </pre>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
